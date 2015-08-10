87ab65e6d6f5a283593b54dcb58248574771bc9e
arthuroconh <arthuro_b@outlook.com>
2015-08-10 16:51:31 -0430

    fgfdg
    dfgfdg

--- www/js/index.js
+++ www/js/index.js
@@ -31,11 +31,9 @@ var app = {
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
+    // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
-        // set to either landscape
-        screen.lockOrientation('portrait');
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
@@ -47,5 +45,60 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
+        var pushNotification = window.plugins.pushNotification;
+        if (device.platform == 'android' || device.platform == 'Android') {
+            //tu Project ID aca!!
+            pushNotification.register(this.successHandler, this.errorHandler,{"senderID":"568956761804","ecb":"app.onNotificationGCM"});
+        }
+        else {
+            pushNotification.register(this.successHandler,this.errorHandler,{"badge":"true","sound":"true","alert":"true","ecb":"app.onNotificationAPN"});
+        }
+    },
+    // result contains any message sent from the plugin call
+    successHandler: function(result) {
+    },
+    errorHandler:function(error) {
+        alert(error);
+    },
+    onNotificationGCM: function(e) {
+        switch( e.event )
+        {
+            case 'registered':
+                if ( e.regid.length > 0 )
+                {
+                    console.log("Regid " + e.regid);
+                    //Cuando se registre le pasamos el regid al input
+                    document.getElementById('regId').value = e.regid;
+                }
+            break;
+
+            case 'message':
+              // NOTIFICACION!!!
+              alert('Nueva Notificacion : '+e.message+'');
+            break;
+
+            case 'error':
+              alert('GCM error = '+e.msg);
+            break;
+
+            default:
+              alert('An unknown GCM event has occurred');
+              break;
+        }
+    },
+    onNotificationAPN: function(event) {
+        var pushNotification = window.plugins.pushNotification;
+        alert("Running in JS - onNotificationAPN - Received a notification! " + event.alert);
+
+        if (event.alert) {
+            navigator.notification.alert(event.alert);
+        }
+        if (event.badge) {
+            pushNotification.setApplicationIconBadgeNumber(this.successHandler, this.errorHandler, event.badge);
+        }
+        if (event.sound) {
+            var snd = new Media(event.sound);
+            snd.play();
+        }
     }
 };
